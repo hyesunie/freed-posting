c4f9dd66680459cb0e7751bb98e656c66e539457
hyesunie <hyesunie1025@gmail.com>
2022-07-21 14:57:27 +0900

    feat: styled-reset 설치 후 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import * as React from 'react';
+import { Reset } from 'styled-reset';
 import './App.css';
 
 function App(): ReactElement  {
   return (
-    <div className="App" />
+    <>
+    <Reset />
+    <div>Hello</div>
+  </>
   );
 }
 
