a3f42bae5c810478044615bbee30f5fe9a4b057e
hyesunie <hyesunie1025@gmail.com>
2022-07-21 14:39:00 +0900

    feat: app Init

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+interface ReactElement<
+  P = any,
+  T extends string | JSXElementConstructor<any> =
+    | string
+    | JSXElementConstructor<any>
+> {
+  type: T;
+  props: P;
+  key: Key | null;
+}
