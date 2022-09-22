167d4d91dda2c606feaa3baa57d3bf33f79ea63d
hyesunie <hyesunie1025@gmail.com>
2022-09-22 22:52:16 +0900

    feat: root 화면 구성 변경

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Form, Link } from 'react-router-dom';
 import { readPostInfo } from '../api';
 
 export interface PostInfoData {
@@ -21,6 +21,23 @@ export const loader = async (): Promise<PostInfoData[]> => {
 function Root(): ReactElement {
   return (
     <div>
+      <div id="sidebar">
+        <section>
+          <form id="search-form" role="search">
+            <input placeholder="Search" type="search" />
+          </form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </section>
+        <nav>
+          <ul>
+            <li>
+              <Link to="posting/1">전체 포스팅 보기</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <main>
         <Outlet />
       </main>
